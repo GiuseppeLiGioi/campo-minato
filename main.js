b38edd7f6a71bbf31b3a31c504e1b4f650a4e0bf
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-09-17 18:40:58 +0200

    added. all the logic that allows the game to function correctly.

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
+let bombs = []
+
+while (bombs.length < 16) {
+    let n = Math.floor(Math.random() * 49) + 1;
+    if (!bombs.includes(n)) {
+        bombs.push(n)
+    }
+}
+console.log(bombs)
+
+
+
+
+
 let numbers = [];
 
 while (numbers.length < 49) {
     let n = Math.floor(Math.random() * 49) + 1;
-    if(!numbers.includes(n)){
+    if (!numbers.includes(n)) {
         numbers.push(n)
     }
 }
@@ -12,16 +26,31 @@ console.log(numbers)
 
 const containerCells = document.getElementById("container-cells")
 
-for(let i = 0; i < numbers.length; i++){
+let score = 0;
+let counter = document.getElementById("counter")
+let gameOver = false;
+
+for (let i = 0; i < numbers.length; i++) {
+    let numeroCella = i;
     let div = document.createElement("div");
     div.classList.add("cell")
     div.innerHTML = numbers[i];
 
     div.addEventListener("click", () => {
-        if(div.classList.contains("clicked")){
+        if (div.classList.contains("clicked")) {
             return;
-        }else{
+        } else {
             div.classList.add("clicked")
+
+
+            if (verifyValue(bombs, numeroCella)) {
+                div.innerHTML = "💣"
+                div.classList.add("bomba")
+                gameOver = true;
+            } else {
+                score++;
+                counter.innerHTML = `${"Contatore:" + score}`
+            }
         }
     })
     containerCells.appendChild(div)
@@ -29,14 +58,16 @@ for(let i = 0; i < numbers.length; i++){
 
 
 
-function verifyValue(arr, num){
-    if(arr.includes(num)){
-        console.log("trovato")
-    }else{
-        console.log("Non trovato")
+function verifyValue(arr, num) {
+    if (arr.includes(num)) {
+        return true
+    } else {
+        return false
     }
 }
 verifyValue(numbers, 70)
+
+
 
 
 
